2c16f288298589fa3f3389e6864ec35069723295
Cássio Quintana <cassioquint@gmail.com>
2025-04-10 22:16:11 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
+            <h1 className="text-3xl font-bold">PokerFace</h1><br/>
+            <h4>Mantenha a calma. Finja que sabe.</h4>
+        </div>
+    );
+}
+
+export default App;
